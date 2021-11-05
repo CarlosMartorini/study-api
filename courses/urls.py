f72072b0baf89b0dc07911a310eb1aac1c6a8c6d
Carlos Martorini <c7.silveira@gmail.com>
2021-11-04 23:20:58 -0300

    Created courses urls

---
+++ courses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import View, ViewById, Create
+
+
+urlpatterns = [
+    path('courses/', View.as_view()),
+    path('courses/<int:course_id>', ViewById.as_view()),
+    path('courses/<int:course_id/registrations>', Create.as_view()),
+]
